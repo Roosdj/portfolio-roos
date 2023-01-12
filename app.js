a2cb69893324d7aacd14d2c363d710d6c5901e70
Roos de Jonge <rosemariedejonge@gmail.com>
2023-01-12 15:55:59 +0100

    Added clickable resume

---
+++ app.js
@@ -0,0 +1,8 @@
+function handleClick() {
+  const showCV = document.getElementById("showCV");
+  if (showCV.style.display === "none") {
+    showCV.style.display = "block";
+  } else {
+    showCV.style.display = "none";
+  }
+}
